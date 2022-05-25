c181fb465eede825447b8dfe91270f3139afa9a5
tonykipkemboi <tonykip@seas.upenn.edu>
2022-05-25 19:10:53 -0400

    commit to repo

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,75 +1,19 @@
 import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  Contract,
-  LogRefund,
-  CreateBAT,
-  Transfer,
-  Approval
-} from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import { Transfer } from "../generated/Contract/Contract"
+import { TransferEvent } from "../generated/schema"
 
-export function handleLogRefund(event: LogRefund): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleTransfer(event: Transfer): void {
+  let transferEvent = new TransferEvent(event.transaction.hash.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  let amount = (event.params._value.toBigDecimal())
+  transferEvent.amount = amount
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+  transferEvent.sender = event.params._from
+  transferEvent.destination = event.params._to
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  transferEvent.block = event.block.number
+  transferEvent.timestamp = event.block.timestamp
+  transferEvent.transaction = event.transaction.hash
 
-  // Entity fields can be set based on event parameters
-  entity._to = event.params._to
-  entity._value = event.params._value
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.batFundDeposit(...)
-  // - contract.name(...)
-  // - contract.approve(...)
-  // - contract.totalSupply(...)
-  // - contract.batFund(...)
-  // - contract.transferFrom(...)
-  // - contract.decimals(...)
-  // - contract.tokenExchangeRate(...)
-  // - contract.version(...)
-  // - contract.tokenCreationCap(...)
-  // - contract.balanceOf(...)
-  // - contract.isFinalized(...)
-  // - contract.fundingEndBlock(...)
-  // - contract.symbol(...)
-  // - contract.ethFundDeposit(...)
-  // - contract.transfer(...)
-  // - contract.tokenCreationMin(...)
-  // - contract.fundingStartBlock(...)
-  // - contract.allowance(...)
+  transferEvent.save()
 }
-
-export function handleCreateBAT(event: CreateBAT): void {}
-
-export function handleTransfer(event: Transfer): void {}
-
-export function handleApproval(event: Approval): void {}
